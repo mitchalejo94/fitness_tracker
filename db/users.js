648abc2fd543606b93c99cb0fa471599c07bd754
cusmoritz <cus.moritz@gmail.com>
2022-12-07 15:39:58 -0700

    feat: createUsers

--- db/users.js
+++ db/users.js
@@ -4,7 +4,21 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
-  
+  // putting a new user into the database
+  try {
+    const {rows: newUser} = await client.query(`
+    INSERT INTO users (username, password)
+    VALUES ($1, $2)
+    RETURNING *
+    ;
+    `, [username, password]);
+    // console.log('new user here', newUser.rows)
+    return newUser;
+  } catch (error) {
+    console.log('there was an error creating a new user in users.js: ', error);
+    throw (error);
+  }
+
 }
 
 async function getUser({ username, password }) {
