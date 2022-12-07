f4f0c3509f935211f54d419e7612aaf1f63db160
cusmoritz <cus.moritz@gmail.com>
2022-12-07 15:49:25 -0700

    feat: getUserById

--- db/users.js
+++ db/users.js
@@ -22,11 +22,40 @@ async function createUser({ username, password }) {
 }
 
 async function getUser({ username, password }) {
+  // getting 1 user from the database
+  /*
 
+    WE MAY HAVE TO CHANGE THIS TO INCLUDE THE PASSWORD FIELD
+
+  */
+  try {
+    const {rows: [fetchUser]} = await client.query(`
+    SELECT * FROM users
+    WHERE username=$1
+    ;
+    `, [username]);
+    // console.log('have fetched a user: ', fetchUser);
+    return fetchUser;
+  } catch (error) {
+    console.log('there was an error getting a user in users/getUser: ', error);
+    throw (error);
+  }
 }
 
 async function getUserById(userId) {
-
+  // getting a user by their username
+  try {
+    const {rows: [getUserById]} = await client.query(`
+    SELECT * FROM users
+    WHERE id=$1
+    ;
+    `, [userId]);
+    console.log('we have gotten a user by their id: ', getUserById);
+    return getUserById;
+  } catch (error) {
+    console.log('there was an error in getUserById: ', error);
+    throw error;
+  }
 }
 
 async function getUserByUsername(userName) {
