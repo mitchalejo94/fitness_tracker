aba0573def44cfdcaf9d2026852f33e8b6368e06
Cindy Carrillo <carrillo201994@gmail.com>
2022-12-09 15:42:09 -0600

    fix: trying to fix merge problems

--- db/users.js
+++ db/users.js
@@ -1,7 +1,7 @@
 const client = require("./client");
 
 // require our hashing function
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt')
 
 // create our hash function
 const SALT_COUNT = 10;
@@ -12,7 +12,7 @@ const SALT_COUNT = 10;
 async function createUser({ username, password }) {
   // putting a new user into the database
   if (!username || !password) {
-    throw new "You must include both a username and a password"();
+    throw new ("You must include both a username and a password");
   }
 
   // hashed password
@@ -22,48 +22,37 @@ async function createUser({ username, password }) {
   // console.log(typeof username);
   // console.log(typeof hashedPass);
   try {
-    const {
-      rows: [newUser],
-    } = await client.query(
-      `
+    const {rows: [newUser]} = await client.query(`
     INSERT INTO users (username, password)
     VALUES ($1, $2)
     RETURNING *
     ;
-    `,
-      [username, hashedPass]
-    );
+    `, [username, hashedPass]);
     // console.log('new user here', newUser)
     delete newUser.password;
     return newUser;
   } catch (error) {
-    console.log("there was an error creating a new user in users.js: ", error);
-    throw error;
+    console.log('there was an error creating a new user in users.js: ', error);
+    throw (error);
   }
 }
 
 // getting 1 user from the database
 async function getUser({ username, password }) {
   /*
-
     WE MAY HAVE TO CHANGE THIS TO INCLUDE THE PASSWORD FIELD
-
   */
   try {
-    const {
-      rows: [fetchUser],
-    } = await client.query(
-      `
+    const {rows: [fetchUser]} = await client.query(`
     SELECT * FROM users
     WHERE username=$1
     ;
-    `,
-      [username]
-    );
+    `, [username]);
     // console.log('have fetched a user: ', fetchUser);
+    
 
     if (!fetchUser) {
-      throw new "Password or username did not match"();
+      throw new ("Password or username did not match");
     } else {
       const passwordcheck = await bcrypt.compare(password, fetchUser.password);
       if (passwordcheck === true) {
@@ -72,50 +61,40 @@ async function getUser({ username, password }) {
       }
     }
   } catch (error) {
-    console.log("there was an error getting a user in users/getUser: ", error);
-    throw error;
+    console.log('there was an error getting a user in users/getUser: ', error);
+    throw (error);
   }
-}
+};
 
 async function getUserById(userId) {
   // getting a user by their username
   try {
-    const {
-      rows: [getUserById],
-    } = await client.query(
-      `
+    const {rows: [getUserById]} = await client.query(`
     SELECT * FROM users
     WHERE id=$1
     ;
-    `,
-      [userId]
-    );
+    `, [userId]);
     // console.log('we have gotten a user by their id: ', getUserById);
     delete getUserById.password;
     return getUserById;
   } catch (error) {
-    console.log("there was an error in getUserById: ", error);
+    console.log('there was an error in getUserById: ', error);
     throw error;
   }
-}
+};
 
 async function getUserByUsername(userName) {
   // getting a user by their username
   try {
-    const {
-      rows: [fetchUserByUsername],
-    } = await client.query(
-      `
+    const {rows: [fetchUserByUsername]} = await client.query(`
     SELECT * FROM users
     WHERE username=$1;
-    `,
-      [userName]
-    );
-    console.log("we have gotten a user by username: ", fetchUserByUsername);
+    `, [userName]);
+    console.log('we have gotten a user by username: ', fetchUserByUsername);
     return fetchUserByUsername;
   } catch (error) {
-    console.log("there was an error in getUserByUsername: ", error);
-    throw error;
+    console.log('there was an error in getUserByUsername: ', error);
+    throw (error);
   }
 }
 
@@ -124,4 +103,4 @@ module.exports = {
   getUser,
   getUserById,
   getUserByUsername,
-};
+}
