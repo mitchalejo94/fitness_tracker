2073fc777a25747d59cac165feb2986022004c61
cusmoritz <cus.moritz@gmail.com>
2022-12-10 09:17:22 -0700

    feat: added getRoutineActivityById

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -1,6 +1,17 @@
 const client = require('./client')
 
 async function getRoutineActivityById(id){
+  try {
+    const {rows: [routineActivity]} = await client.query(`
+    SELECT * FROM routine_activities
+    WHERE id=$1;
+    `, [id]);
+    console.log('here we have a routine by Id: ', routineActivity);
+    return routineActivity;
+  } catch (error) {
+    console.log('there was an error in getRoutineActivityById: ', error);
+    throw error;
+  }
 }
 
 async function addActivityToRoutine({
