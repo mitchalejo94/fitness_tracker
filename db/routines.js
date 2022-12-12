0acc07f6cd8449781f329b736891fa8d634b5d58
Cindy Carrillo <carrillo201994@gmail.com>
2022-12-12 10:49:14 -0600

    attempting to complete functions

--- db/routines.js
+++ db/routines.js
@@ -1,8 +1,10 @@
 const client = require("./client");
-const { createUser,
+const {
+  createUser,
   getUser,
   getUserById,
-  getUserByUsername,} = require("./users.js")
+  getUserByUsername,
+} = require("./users.js");
 
 //SMALL CHANGE
 
@@ -57,36 +59,107 @@ async function getAllRoutines() {
   }
 }
 
-async function getAllRoutinesByUser({ username }) {}
+async function getAllRoutinesByUser({ username }) {
+  try {
+    const { id } = await getUserByUsername(username);
+    const { rows: routines } = await client.query(`
+    SELECT routines.*, users.username AS "creatorName" 
+    FROM routines 
+    INNER JOIN users ON routines."creatorId"=users.id 
+    WHERE "creatorId"=${id};
+    `);
 
-async function getPublicRoutinesByUser({ username }) {}
+    const routinesWithActivities = await Promise.all(
+      routines.map(async (routine) => {
+        const { rows: activities } = await client.query(`
+        SELECT routine_activities.*, routine_activities.id as "routineActivityId", activities.* 
+        FROM routine_activities 
+        INNER JOIN activities ON routine_activities."activityId"=activities.id 
+        WHERE routine_activities."routineId"=${routine.id};
+        `);
+        return { ...routine, activities };
+      })
+    );
+
+    return routinesWithActivities;
+  } catch (error) {
+    console.log("There was an error in getting all routines by user", error);
+    throw error;
+  }
+}
+
+async function getPublicRoutinesByUser({ username }) {
+  try {
+    const { id } = await getUserByUsername(username);
+    const { rows: routines } = await client.query(`
+    SELECT routines.*, users.username AS "creatorName" 
+    FROM routines 
+    INNER JOIN users ON routines."creatorId"=users.id 
+    WHERE "creatorId"=${id} AND "isPublic"=true;
+      
+    `);
+
+    const routinesWithActivities = await Promise.all(
+      routines.map(async (routine) => {
+        const { rows: activities } = await client.query(`
+        SELECT routine_activities.*, routine_activities.id as "routineActivityId", activities.* 
+        FROM routine_activities 
+        INNER JOIN activities ON routine_activities."activityId"=activities.id 
+        WHERE routine_activities."routineId"=${routine.id};
+        `);
+        return { ...routine, activities };
+      })
+    );
+
+    return routinesWithActivities;
+  } catch (error) {
+    console.log("There was an error in getting public routines by user", error);
+    throw error;
+  }
+}
 
 async function getAllPublicRoutines(isPublic) {
-  
   try {
-  const {
-    rows: [routine],
-  } = await client.query(
-    `
+    const {
+      rows: [routine],
+    } = await client.query(
+      `
     SELECT * 
     FROM routines
     WHERE "isPublic"=TRUE;
     `,
-    [isPublic]
-  );
+      [isPublic]
+    );
 
-  return routine;
-} catch (error) {
-  console.log("There was an error getting routine by Id", error);
-  throw error;
-}}
+    return routine;
+  } catch (error) {
+    console.log("There was an error getting routine by Id", error);
+    throw error;
+  }
+}
 
-async function getPublicRoutinesByActivity({ id }) {}
+async function getPublicRoutinesByActivity({ id }) {
+  try {
+    const publicRoutines = (await getAllPublicRoutines()).filter((routine) => {
+      for (let activity of routine.activities) {
+        if (activity.activityId === id) return true;
+      }
+    });
 
- async function createRoutine({ creatorId, isPublic, name, goal }) {
+    return publicRoutines;
+  } catch (error) {
+    console.log(
+      "There was an error getting public routines by activity",
+      error
+    );
+    throw error;
+  }
+}
+
+async function createRoutine({ creatorId, isPublic, name, goal }) {
   try {
     const {
-      rows: [routine]
+      rows: [routine],
     } = await client.query(
       `
       INSERT INTO routines ("creatorId", "isPublic", name, goal)
@@ -96,14 +169,12 @@ async function getPublicRoutinesByActivity({ id }) {}
       [creatorId, isPublic, name, goal]
     );
 
-   
     return routine;
   } catch (error) {
     console.log("There was an error creating a routine", error);
     throw error;
   }
 }
-
 
 async function updateRoutine({ id, creatorId, isPublic, name, goal }) {
   try {
@@ -126,18 +197,20 @@ async function updateRoutine({ id, creatorId, isPublic, name, goal }) {
 }
 
 async function destroyRoutine(id) {
-  try { 
-    const { 
-      rows: [routine]
+  try {
+    const {
+      rows: [routine],
     } = await client.query(
-        `
+      `
         DELETE FROM ROUTINES
         WHERE id=$1
         `,
-        [id]
+      [id]
     );
-  } catch (error){
-    console.log("unable to delete routine", error)
+
+    return routine;
+  } catch (error) {
+    console.log("unable to delete routine", error);
     throw error;
   }
 }
