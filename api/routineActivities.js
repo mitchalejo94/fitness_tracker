f7b549d12424c0e00afea0f5aec95060f3929125
Martin Hampton <martinh0205@gmail.com>
2022-12-17 11:37:46 -0600

    started file

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const {
+    getRoutineActivityById,
+    addActivityToRoutine,
+    getRoutineActivitiesByRoutine,
+    updateRoutineActivity,
+    destroyRoutineActivity,
+} = require ("../db/routine_activities") 
 
 // PATCH /api/routine_activities/:routineActivityId
 
 // DELETE /api/routine_activities/:routineActivityId
+/* router.delete("/:routineActivityId", async (req, res, next) => {
+    try {
+        const { getRoutineActivityById } = req.params;
+        const { destroyRoutineActivity } 
 
+    }
+})
+
+*/
 module.exports = router;
