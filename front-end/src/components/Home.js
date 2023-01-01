baf564e4e4619996a149896de6c11b406cda4908
cusmoritz <cus.moritz@gmail.com>
2023-01-01 12:27:41 -0700

    feat: fixed login / register function. Pushes to '/' upon sign in

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -10,12 +10,14 @@ const Home = ({token}) => {
     }
     // const data = await getUserData(token);
     // console.log('datadatadata: ', data)
+    getUserData();
     return (
         <>
         <div className="ui container">
         <h1 className="centered ui header">Welcome Home</h1>
         {token ? 
-            (`You are logged in as ${data.username}`) 
+            // (`You are logged in as ${data.username}`) 
+            console.log('you are logged in')
         : 
             (<p>You are not logged in.</p>)}
         </div>
