744b5920f2eb7505b80b00038586a4ad700657fb
Mitchel Alejo <michalejo94@gmail.com>
2023-01-02 16:30:30 -0600

    fixing merge discrepencies

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,13 +1,24 @@
 import react from "react";
 
 
-const Home = ({username}) => {
-    console.log("username", username)
+const Home = ({token}) => {
+    const getUserData = async (token) => {
+        const data = await fetchUser(token);
+        console.log('this is data in Home: ', data);
+        return data;
+    }
+    // const data = await getUserData(token);
+    // console.log('datadatadata: ', data)
+    getUserData();
     return (
         <>
         <div className="ui container">
         <h1 className="centered ui header">Welcome Home</h1>
-       
+        {token ?
+            // (`You are logged in as ${data.username}`)
+            console.log('you are logged in')
+        :
+            (<p>You are not logged in.</p>)}
         </div>
         </>
     )
