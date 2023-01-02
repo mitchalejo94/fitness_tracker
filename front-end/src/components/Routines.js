52772bc4296d58d5fb50e98b679b33f16beabe16
Martin Hampton <martinh0205@gmail.com>
2023-01-01 20:29:34 -0600

    rendered routines styled page

--- front-end/src/components/Routines.js
+++ front-end/src/components/Routines.js
@@ -1,29 +1,71 @@
 import react from "react";
 
-
-const Routines = ({routines, token}) => {
-    return (
+const Routines = ({ routines, token }) => {
+  return (
+    <>
+      <h1 className="centered ui header">Routines</h1>
+      {token ? (
         <>
-        <h1 className="centered ui header">Routines</h1>
-        {token ? <> <div className="ui card centered"><div className="content"><div className="header">CREATE ROUTINE</div><form class="ui form"><div class="field"><label>Routine Name</label><input placeholder="Routine Name"/></div><div class="field"><label>Routine Description</label><input placeholder="Activity Description"/></div><div class="field"></div><button class="ui button" type="submit">Create Routine</button></form></div> </div> </> : null}
-        {routines.map((individualRoutine)=> {
-            return( 
-                <>
-                
-    
-                <div className="ui card centered">
-                
-                    <div className="header">{individualRoutine.name.toUpperCase()}</div>
-                    <div className="meta">{individualRoutine.goal}</div>
-                
-                    </div>
-                </>
-            )
-        })}
+          {" "}
+          <div className="ui card centered">
+            <div className="content">
+              <div className="header">CREATE ROUTINE</div>
+              <form class="ui form">
+                <div class="field">
+                  <label>Routine Name</label>
+                  <input placeholder="Routine Name" />
+                </div>
+                <div class="field">
+                  <label>Routine Description</label>
+                  <input placeholder="Activity Description" />
+                </div>
+                <div class="field"></div>
+                <button class="ui button" type="submit">
+                  Create Routine
+                </button>
+              </form>
+            </div>{" "}
+          </div>{" "}
         </>
-    )
-}
-
-
+      ) : null}
+      {routines.map((individualRoutine) => {
+        return (
+          <>
+            <div className="ui card centered">
+              <div className="card header">
+                {individualRoutine.name.toUpperCase()}
+              </div>
+              <div classname="meta">
+                <span className="goal">By: </span>
+                {individualRoutine.creatorName}
+              </div>
+              <div className="description">
+                <span className="goal">Goal: </span>
+                {individualRoutine.goal}
+              </div>
+              <div className="description">
+                <span className="goal">Activities included: </span>
+              </div>
+              {individualRoutine.activities.map((activities) => {
+                return (
+                  <>
+                    <div className="description">
+                      {activities.name}(x{activities.duration})
+                    </div>
+                  </>
+                );
+              })}
+              <span>
+                {" "}
+                <button className="ui right floated button">Delete</button>{" "}
+                <button className="ui right floated button">Edit</button>
+              </span>
+            </div>
+          </>
+        );
+      })}
+    </>
+  );
+};
 
 export default Routines;
