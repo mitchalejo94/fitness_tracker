392a97049a392de78b9e7a1f6181b66faab03ed3
Martin Hampton <martinh0205@gmail.com>
2023-01-02 19:45:13 -0600

    started routines again

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -1,7 +1,6 @@
 // const { post } = require("../../../api/routines");
 
-
- const URL = "https://fitnesstrac-kr.herokuapp.com/api";
+const URL = "https://fitnesstrac-kr.herokuapp.com/api";
 
 //  const makeHeaders = (token)=>{
 //     const headers = {
@@ -15,15 +14,15 @@
 
 //  const callAPI = async (endpointPath, defaultOptions = {}) =>{
 //     const {token, method, body} = defaultOptions;
-    
+
 //     const options = {
 //         headers: makeHeaders(token)
 //     }
 //     if(method){
-//         options.method = method 
+//         options.method = method
 //     }
 //     if(body){
-//         options.body = JSON.stringify(body)   
+//         options.body = JSON.stringify(body)
 //     }
 //     const response = await fetch (`${URL}${endpointPath}`, options)
 //     const result = await response.json()
@@ -31,202 +30,195 @@
 //     return result
 //  }
 
- export const fetchActivities = async() => {
-    try {
-        const response = await fetch(`${URL}/activities`, {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          }).then(response => response.json()) 
-            return response
-    } catch ( error) {
-        console.error ("There was an error fetching the activities", error)
+export const fetchActivities = async () => {
+  try {
+    const response = await fetch(`${URL}/activities`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => response.json());
+    return response;
+  } catch (error) {
+    console.error("There was an error fetching the activities", error);
+  }
+};
+
+export const registerUser = async (username, password) => {
+  try {
+    const response = await fetch(`${URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    const result = await response.json();
+    console.log("result.....", result);
+    return result;
+  } catch (error) {
+    console.error("There was an error registering the user", error);
+  }
+};
+
+export const loginUser = async (username, password) => {
+  try {
+    const response = await fetch(`${URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("There was an error logging in the user", error);
+    throw error;
+  }
+};
+
+export const fetchUser = async (token) => {
+  // console.log('token in fetchuser: ', token)
+  try {
+    const response = await fetch(`${URL}/users/me`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((response) => response.json());
+    // console.log('this is fetch response: ', response);
+    return response;
+  } catch (error) {
+    console.log("there was an error fetching the user: ", error);
+    throw error;
+  }
+};
+
+export const fetchRoutines = async (username, token) => {
+  try {
+    if (token) {
+      const response = await fetch(`${URL}/users/${username}/routines`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const routines = response.json();
+      return routines;
+    } else {
+      const response = await fetch(`${URL}/users/${username}/routines`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const routines = response.json();
+      return routines;
     }
- };
+  } catch (error) {
+    console.log("there was an error fetching routines: ", error);
+    throw error;
+  }
+};
 
-
- 
- export const registerUser = async (username, password) => {
-    try{
-        const response = await fetch(`${URL}/users/register`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username,
-                password,
-            })
-        })
-        const result = await response.json()
-        console.log("result.....", result)
-        return result
-    }catch(error) {
-        console.error("There was an error registering the user", error)
-    }
- }
-
- export const loginUser = async (username, password) => {
-    try {
-        const response = await fetch(`${URL}/users/login`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username,
-                password,
-            })
-        })
-        const result = await response.json()
-        return result
-    } catch (error) {
-        console.error("There was an error logging in the user", error)
-        throw error;
-    }
- }
-
- export const fetchUser = async (token) => {
-    // console.log('token in fetchuser: ', token)
-    try {
-        const response = await fetch(`${URL}/users/me`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            }).then(response => response.json());
-            // console.log('this is fetch response: ', response);
-            return response;
-    } catch (error) {
-        console.log('there was an error fetching the user: ', error);
-        throw error;
-    }
- }
-
-
- export const fetchRoutines = async(username, token) => {
-    try {
-        if (token) { 
-            const response = await fetch(`${URL}/users/${username}/routines`, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            const routines = response.json();
-            return routines;
-        } else {
-            const response = await fetch(`${URL}/users/${username}/routines`, {
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            });
-            const routines = response.json();
-            return routines;
-        }
-    } catch (error) {
-        console.log('there was an error fetching routines: ', error);
-        throw error;
-    }
- };
-
- // this function lets us make a new routine
- export const postRoutine = async(name, goal, visability, token) => {
-    try {
-        // console.log('our info inside the post method: ', name, goal, visability, token)
-        const gatheringData = await fetch(`${URL}/routines`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                    name: name,
-                    goal: goal,
-                    isPublic: visability
-                })
-        });
-        const newRoutine = await gatheringData.json();
-        console.log('our new routine: ', newRoutine)
-        return newRoutine;
-    } catch (error) {
-        console.log('there was an erro creating a new routine: ', error);
-        throw error;
-    }
-}
+// this function lets us make a new routine
+export const postRoutine = async (name, goal, visability, token) => {
+  try {
+    // console.log('our info inside the post method: ', name, goal, visability, token)
+    const gatheringData = await fetch(`${URL}/routines`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name: name,
+        goal: goal,
+        isPublic: visability,
+      }),
+    });
+    const newRoutine = await gatheringData.json();
+    console.log("our new routine: ", newRoutine);
+    return newRoutine;
+  } catch (error) {
+    console.log("there was an erro creating a new routine: ", error);
+    throw error;
+  }
+};
 
 // this function lets us PATCH a routine
 export const patchRoutine = async (name, goal, isPublic, token, routineId) => {
-    // PATCH /api/routines/:routineId
-    try {
-        const gatheringData = await fetch(`${URL}/routines/${routineId}`, {
-            method: "PATCH",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                name: name,
-                goal: goal,
-                isPublic: isPublic,
-            })
-        });
+  // PATCH /api/routines/:routineId
+  try {
+    const gatheringData = await fetch(`${URL}/routines/${routineId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name: name,
+        goal: goal,
+        isPublic: isPublic,
+      }),
+    });
 
-        const editedRoutine = await gatheringData.json();
-        return editedRoutine;
-    } catch (error) {
-        console.log('there was an error patching a routine: ', error);
-        throw error;
-    }
-}
+    const editedRoutine = await gatheringData.json();
+    return editedRoutine;
+  } catch (error) {
+    console.log("there was an error patching a routine: ", error);
+    throw error;
+  }
+};
 
 // this api call will hard delete a routine from the database
 export const deleteRoutine = async (routineId, token) => {
-    try {
-        const gatheringData = await fetch(`${URL}/routines/${routineId}`, {
-            method: "DELETE",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
-        });
-        const deletedRoutine = await gatheringData.json();
-        console.log('this routine was delete: ', deletedRoutine);
-        return deletedRoutine;
-    } catch (error) {
-        console.log('there was an error deleting a routine: ', error);
-        throw error;
-    }
-}
+  try {
+    const gatheringData = await fetch(`${URL}/routines/${routineId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const deletedRoutine = await gatheringData.json();
+    console.log("this routine was delete: ", deletedRoutine);
+    return deletedRoutine;
+  } catch (error) {
+    console.log("there was an error deleting a routine: ", error);
+    throw error;
+  }
+};
 
- export const createActivities = async (  name, description, token) => {
-    try {
-        const response = await fetch(`${URL}/activities`,
-        {
-             
-            method: "POST",
-            headers:{
-                'Content-Type': 'application/json',
-                'Authorization':`Bearer ${token}`
-            },
-            body: JSON.stringify({
-                name,
-                description,
-                token,
-        
-            })
-        })
-        const result = await response.json()
-        console.log(token, "api token");
-        console.log(name, "API NAME");
-        console.log(description, 'API DESCRIPTION');
-        console.log("createActivities api call result:", result)
-        return result
-    } catch (error) {
-        console.error("There was an error creating a new activity", error)
-    }
- }
-
+export const createActivities = async (name, description, token) => {
+  try {
+    const response = await fetch(`${URL}/activities`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name,
+        description,
+        token,
+      }),
+    });
+    const result = await response.json();
+    console.log(token, "api token");
+    console.log(name, "API NAME");
+    console.log(description, "API DESCRIPTION");
+    console.log("createActivities api call result:", result);
+    return result;
+  } catch (error) {
+    console.error("There was an error creating a new activity", error);
+  }
+};
 
 //  export const fetchActivities = async ()=>{
 //     try{
@@ -254,4 +246,4 @@ export const deleteRoutine = async (routineId, token) => {
 //         }
 //     }
 //  }
-export default loginUser
+export default loginUser;
--- front-end/src/components/Routines.js
+++ front-end/src/components/Routines.js
@@ -1,14 +1,48 @@
 import react from "react";
 
+const Routines = (routines) => {
+  return (
+    <>
+      <h1 className="centered ui header">Routines</h1>
 
-const Routines = () => {
-    return (
-        <>
-        <h1 className="centered ui header">Routines</h1>
-        </>
-    )
-}
-
-
+      {routines.map((individualRoutine) => {
+        return (
+          <>
+            <div className="ui card centered">
+              <div className="card header">
+                {individualRoutine.name.toUpperCase()}
+              </div>
+              <div classname="meta">
+                <span className="goal">By: </span>
+                {individualRoutine.creatorName}
+              </div>
+              <div className="description">
+                <span className="goal">Goal: </span>
+                {individualRoutine.goal}
+              </div>
+              <div className="description">
+                <span className="goal">Activities included: </span>
+              </div>
+              {individualRoutine.activities.map((activities) => {
+                return (
+                  <>
+                    <div className="description">
+                      {activities.name}(x{activities.duration})
+                    </div>
+                  </>
+                );
+              })}
+              <span>
+                {" "}
+                <button className="ui right floated button">Delete</button>{" "}
+                <button className="ui right floated button">Edit</button>
+              </span>
+            </div>
+          </>
+        );
+      })}
+    </>
+  );
+};
 
 export default Routines;
